d5f0d6e4f4b349ee2bf9f1698bf02f45f41bf8b1
Tran Duy Khang <tranduykhang1999@gmail.com>
2020-07-17 20:47:34 +0700

    install dependency

--- DB/db.js
+++ DB/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: 'db4free.net',
-    port: '3306',
-    user: 'duykhang',
-    password: 'duykhang010',
-    database: 'do_an1700215'
+	host: 'db4free.net',
+	port: '3306',
+	user: 'duykhang',
+	password: 'duykhang010',
+	database: 'do_an1700215'
 })
 
 
