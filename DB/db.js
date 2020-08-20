d1078431d689733fc0c25929e9ec4ad151886b68
Tran Duy Khang <tranduykhang1999@gmail.com>
2020-08-20 20:21:21 +0700

    change database

--- DB/db.js
+++ DB/db.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
 
+ /* const conn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'dika'
+})
+*/
+
 const conn = mysql.createConnection({
 	host: 'db4free.net',
-	port: '3306',
+   port: '3306',
 	user: 'duykhang',
 	password: 'duykhang010',
 	database: 'do_an1700215'
 })
+
 
 
 conn.connect();
